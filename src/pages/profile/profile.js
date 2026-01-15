92dcfef7f8336d37708449862394315df6097c7a
Tim Rothaemel <timrothamel@gmail.com>
2026-01-16 00:46:04 +0100

    Add login page and implement user authentication check on profile page

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,3 +1,12 @@
 import { printLog } from "../../scripts/output/log/log.js";
+import { checkUserLoggesIn } from "../../scripts/outh/outh-check.js";
 
 printLog("[Profile Page] Initializing Profile Page");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const isLoggedIn = await checkUserLoggesIn();
+    if (!isLoggedIn) {
+        window.location.href = "../login/login.html";
+    }
+
+})
---
+++ src/scripts/outh/outh-check.js
@@ -0,0 +1,24 @@
+import { printLog } from "../output/log/log.js";
+import { throwNewError } from "../output/error/error.js";
+import { supabase } from "../../services/supabase.js";
+
+printLog("[Outh Check] Initializing Outh Check");
+
+export async function checkUserLoggesIn() {
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+  if (error) {
+    throwNewError("[Outh Check] Error getting session:", error);
+    return false;
+  }
+
+  if (session) {
+    printLog("[Outh Check] User is logged in");
+    return true;
+  } else {
+    printLog("[Outh Check] No user logged in");
+    return false;
+  }
+}
